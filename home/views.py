72dadaa8df78f98fd78d5911227f2d8f8c07913f
Rodrigo Parada <rpparada@gmail.com>
2020-05-21 11:41:30 -0400

    versioon beta

--- home/views.py
+++ home/views.py
@@ -5,8 +5,16 @@ from menu.models import promociones, pizza
 # Create your views here.
 def index(request):
 
-    menuIzq = pizza.objects.all().order_by('id')[:6]
-    menuDer = pizza.objects.all().order_by('id')[6:]
+    listacompleta = pizza.objects.all().order_by('id')
+    total = listacompleta.count()
+
+    if total%2 == 0:
+        menuIzq = listacompleta[:total/2]
+        menuDer = listacompleta[total/2:]
+    else:
+        menuIzq = listacompleta[:total//2+1]
+        menuDer = listacompleta[total//2+1:]
+
     promos = promociones.objects.all().order_by('id')
 
     context = {
